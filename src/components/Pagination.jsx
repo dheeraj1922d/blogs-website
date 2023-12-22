723e32c0f74d4514d533a521e7c8193bdd499694
Dhiraj <rd6999787@gmail.com>
2023-12-22 14:58:50 +0530

    Pagination component is added .. css pending

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Pagination = () => {
+  const { page , changePage , totalPage } = useContext(AppContext);
   return (
-    <div>Pagination</div>
+    <div>
+      { 
+      page > 1 && 
+      (
+        <button onClick={()=>changePage(page-1)}>Previous</button>
+      )
+      }
+      {
+        page < totalPage && (
+          <button onClick={()=>changePage(page+1)}>Next</button>
+        )
+      }
+
+      <div>
+        page{page} of {totalPage}
+      </div>
+    </div>
   )
 }
 
